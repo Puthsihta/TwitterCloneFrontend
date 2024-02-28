243f4db9dff637e6ad2c7864dcfd5dbfe30d2cc7
Puthsitha <puthsithamouern@gmail.com>
2024-02-28 14:09:35 +0700

    feat: init home screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      purge: false,
+    },
+    fontFamily: {
+      sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        blue: "#1DA1F2",
+        darkblue: "#2795D9",
+        lightblue: "#EFF9FF",
+        dark: "#657786",
+        light: "#AAB8C2",
+        lighter: "#E1E8ED",
+        lightest: "#F5F8FA",
+      },
+    },
   },
   plugins: [],
-}
-
+};
